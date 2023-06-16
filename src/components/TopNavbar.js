a523a46f38fb2dcee344b032913821662a5e35ac
LeeTaekJoo <11ltj12@naver.com>
2023-06-16 09:09:16 +0900

    사이드바 진행중, 메인css 수정

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -2,7 +2,13 @@ import {
   AppBar,
   Badge,
   Box,
+  Divider,
+  Drawer,
   IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   Menu,
   MenuItem,
   Toolbar,
@@ -17,6 +23,7 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import PersonIcon from "@mui/icons-material/Person";
 import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
 import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+import "../styles/MainCSS/TopNavbar.css";
 
 const GradientAppBar = styled(AppBar)`
   background: linear-gradient(360deg, #17b7bd 4.53%, #7cde9d 75.31%);
@@ -24,6 +31,7 @@ const GradientAppBar = styled(AppBar)`
 
 const TopNavbar = () => {
   const [profileMenuAnchor, setProfileMenuAnchor] = useState(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const isMenuOpen = Boolean(profileMenuAnchor);
 
@@ -39,9 +47,9 @@ const TopNavbar = () => {
 
   const navigator = useNavigate();
 
-  //menuitem 이동
+  // 메뉴 아이템으로 이동
   const handleNavigate = (e) => {
-    //console.log(e.target.dataset.name);
+    // console.log(e.target.dataset.name);
     navigator(e.target.dataset.name);
   };
 
@@ -67,13 +75,68 @@ const TopNavbar = () => {
       </MenuItem>
       <MenuItem onClick={handleNavigate} data-name="/home">
         <SettingsRoundedIcon sx={{ mr: "10px" }} />
-        카드관리
+        카드 관리
       </MenuItem>
       <MenuItem onClick={handleNavigate} data-name="/logout">
         <LogoutRoundedIcon sx={{ mr: "10px" }} />
         로그아웃
       </MenuItem>
     </Menu>
+  );
+
+  const SideBarButtonClick = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const sidebar = (
+    <Drawer anchor="left" open={isSidebarOpen} onClose={closeSidebar}>
+      <Box className="drawerContent">
+        <List>
+          <ListItem button onClick={handleNavigate} data-name="/home">
+            <ListItemIcon>
+              <SettingsRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary="My Card" />
+          </ListItem>
+          <Divider />
+          <ListItem button onClick={handleNavigate}>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary="Event" />
+          </ListItem>
+          <Divider />
+          <ListItem button onClick={handleNavigate}>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary="Community" />
+          </ListItem>
+          <Divider />
+          <ListItem button onClick={handleNavigate}>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary="Account" />
+          </ListItem>
+        </List>
+        <Box className="drawerFooter">
+          <Divider />
+          <List>
+            <ListItem button onClick={handleNavigate} data-name="/logout">
+              <ListItemIcon>
+                <LogoutRoundedIcon />
+              </ListItemIcon>
+              <ListItemText primary="로그아웃" />
+            </ListItem>
+          </List>
+        </Box>
+      </Box>
+    </Drawer>
   );
 
   return (
@@ -86,18 +149,19 @@ const TopNavbar = () => {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 1, color: "white" }}
+            onClick={SideBarButtonClick}
           >
             <MenuIcon />
           </IconButton>
           <Link to="/home">
             <Box>
-              <img src={HeadLogo} alt="탑 Nav바 LOGO" />
+              <img src={HeadLogo} alt="탑 Nav바 로고" />
             </Box>
           </Link>
           <Box sx={{ flexGrow: 1 }} />
           <Box>
             <IconButton size="large" aria-label="알림창">
-              {/* 알림 숫자 표시 여기서 badgeContent안에서 설정*/}
+              {/* 알림 숫자 표시 - badgeContent 안에서 설정 */}
               <Badge badgeContent={5} color="error">
                 <NotificationsIcon sx={{ color: "white" }} />
               </Badge>
@@ -115,6 +179,7 @@ const TopNavbar = () => {
         </Toolbar>
       </GradientAppBar>
       {profileMenu}
+      {sidebar}
     </Box>
   );
 };
