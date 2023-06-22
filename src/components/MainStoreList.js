ec1930aaa428e224f701d57b2e3f4c1d9716a4f5
LeeTaekJoo <11ltj12@naver.com>
2023-06-22 09:21:18 +0900

    가게 리스트 수정, 알림 기능 완료

--- src/components/MainStoreList.js
+++ src/components/MainStoreList.js
@@ -5,10 +5,12 @@ import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
-import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import "../styles/MainCSS/StoreList.css";
+import health1 from "../images/ListImg/healthImg1.png";
+import health2 from "../images/ListImg/bowlinglogo1.png";
+import health3 from "../images/ListImg/baseballlogo2.png";
+import health4 from "../images/ListImg/soccerlogo.png";
+import { Divider } from "@mui/material";
 
 function MainStoreList({ children, value, index }) {
   return (
@@ -59,118 +61,149 @@ function ListTabs() {
       {/* 가게목록 */}
       <MainStoreList value={value} index={0}>
         <Grid container spacing={2} className="gridList">
-          <Grid item xs={6}>
-            <Stack direction="row" alignItems="center">
-              <DirectionsRunIcon sx={{ fontSize: "inherit" }} />
-              <Typography component="span" sx={{ fontSize: "inherit" }}>
-                택주네 헬스장
-              </Typography>
-            </Stack>
+          <Grid
+            item
+            xs={5}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              src={health1}
+              style={{ objectFit: "cover", width: "80%", height: "80%" }}
+              alt="헬스장이미지"
+            />
           </Grid>
-          <Grid item xs={6} className="benefit">
-            <Typography component="span" sx={{ fontSize: "inherit" }}>
-              등급별 혜택 + 2%
+          <Grid
+            item
+            xs={7}
+            className="benefit"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Typography
+              component="span"
+              sx={{
+                fontSize: "inherit",
+                alignItems: "center",
+                marginLeft: "30%",
+              }}
+              align="center"
+            >
+              택주네 헬스장
             </Typography>
           </Grid>
-          <Grid item xs={6}>
-            <Stack direction="row" alignItems="center">
-              <DirectionsRunIcon sx={{ fontSize: "inherit" }} />
-              <Typography component="span" sx={{ fontSize: "inherit" }}>
-                갱윤쓰 볼링장
-              </Typography>
-            </Stack>
+          <Divider sx={{ width: "100%" }} />
+          <Grid
+            item
+            xs={5}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              src={health2}
+              style={{ objectFit: "cover", width: "80%", height: "80%" }}
+              alt="헬스장이미지"
+            />
           </Grid>
-          <Grid item xs={6} className="benefit">
-            <Typography component="span" sx={{ fontSize: "inherit" }}>
-              등급별 혜택 + 4%
+          <Grid
+            item
+            xs={7}
+            className="benefit"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Typography
+              component="span"
+              sx={{
+                fontSize: "inherit",
+                alignItems: "center",
+                marginLeft: "30%",
+              }}
+              align="center"
+            >
+              경윤쓰 볼링장
             </Typography>
           </Grid>
-          <Grid item xs={6}>
-            <Stack direction="row" alignItems="center">
-              <DirectionsRunIcon sx={{ fontSize: "inherit" }} />
-              <Typography component="span" sx={{ fontSize: "inherit" }}>
-                지만이네 풋살장
-              </Typography>
-            </Stack>
+          <Divider sx={{ width: "100%" }} />
+          <Grid
+            item
+            xs={5}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              src={health3}
+              style={{
+                objectFit: "cover",
+                width: "80%",
+                height: "80%",
+                marginBottom: "30px",
+              }}
+              alt="스크린야구장이미지"
+            />
           </Grid>
-          <Grid item xs={6} className="benefit">
-            <Typography component="span" sx={{ fontSize: "inherit" }}>
-              등급별 혜택 + 10%
+          <Grid
+            item
+            xs={7}
+            className="benefit"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Typography
+              component="span"
+              sx={{
+                fontSize: "inherit",
+                alignItems: "center",
+                marginLeft: "30%",
+              }}
+              align="center"
+            >
+              지만쓰 야구장
             </Typography>
           </Grid>
-          <Grid item xs={6}>
-            <Stack direction="row" alignItems="center">
-              <DirectionsRunIcon sx={{ fontSize: "inherit" }} />
-              <Typography component="span" sx={{ fontSize: "inherit" }}>
-                태영이네 야구장
-              </Typography>
-            </Stack>
+          <Divider sx={{ width: "100%" }} />
+          <Grid
+            item
+            xs={5}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              src={health4}
+              style={{ objectFit: "cover", width: "80%", height: "80%" }}
+              alt="풋살장이미지"
+            />
           </Grid>
-          <Grid item xs={6} className="benefit">
-            <Typography component="span" sx={{ fontSize: "inherit" }}>
-              등급별 혜택 + 7%
+          <Grid
+            item
+            xs={7}
+            className="benefit"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Typography
+              component="span"
+              sx={{
+                fontSize: "inherit",
+                alignItems: "center",
+                marginLeft: "30%",
+              }}
+              align="center"
+            >
+              태영쓰 풋살장
             </Typography>
           </Grid>
         </Grid>
       </MainStoreList>
-
-      {/* 식품목록 */}
-      <MainStoreList value={value} index={1}>
-        <Grid container spacing={2} className="gridList">
-          <Grid item xs={6}>
-            <Stack direction="row" alignItems="center">
-              <AddShoppingCartIcon sx={{ fontSize: "inherit" }} />
-              <Typography component="span" sx={{ fontSize: "inherit" }}>
-                랭킹 닭컴
-              </Typography>
-            </Stack>
-          </Grid>
-          <Grid item xs={6} className="benefit">
-            <Typography component="span" sx={{ fontSize: "inherit" }}>
-              등급별 혜택 + 2%
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <Stack direction="row" alignItems="center">
-              <AddShoppingCartIcon sx={{ fontSize: "inherit" }} />
-              <Typography component="span" sx={{ fontSize: "inherit" }}>
-                아임닭
-              </Typography>
-            </Stack>
-          </Grid>
-          <Grid item xs={6} className="benefit">
-            <Typography component="span" sx={{ fontSize: "inherit" }}>
-              등급별 혜택 + 4%
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <Stack direction="row" alignItems="center">
-              <AddShoppingCartIcon sx={{ fontSize: "inherit" }} />
-              <Typography component="span" sx={{ fontSize: "inherit" }}>
-                국민상점
-              </Typography>
-            </Stack>
-          </Grid>
-          <Grid item xs={6} className="benefit">
-            <Typography component="span" sx={{ fontSize: "inherit" }}>
-              등급별 혜택 + 6%
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <Stack direction="row" alignItems="center">
-              <AddShoppingCartIcon sx={{ fontSize: "inherit" }} />
-              <Typography component="span" sx={{ fontSize: "inherit" }}>
-                허닭
-              </Typography>
-            </Stack>
-          </Grid>
-          <Grid item xs={6} className="benefit">
-            <Typography component="span" sx={{ fontSize: "inherit" }}>
-              등급별 혜택 + 5%
-            </Typography>
-          </Grid>
-        </Grid>
-      </MainStoreList>
+      <MainStoreList value={value} index={1}></MainStoreList>
     </Box>
   );
 }
