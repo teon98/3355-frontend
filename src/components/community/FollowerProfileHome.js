096852a3de9f57126c1ac6fca3d551d3f6bd649b
teon98 <k55416325@gmail.com>
2023-06-26 17:59:17 +0900

    다수 사용자 프로필 불러오기

--- src/components/community/FollowerProfileHome.js
+++ src/components/community/FollowerProfileHome.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FollowerProfile from "./FollowerProfile";
+import { Box, Typography } from "@mui/material";
+import ProfileView from "./ProfileView";
+import { useSelector } from "react-redux";
+import ProfilePost from "./ProfilePost";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const FollowerProfileHome = () => {
+  const params = useParams();
+
+  const [userNo, setUserNo] = useState("");
+  //loading
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!userNo) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
+    }
+
+    axios
+      .get("/post/followerPost", {
+        params: {
+          userNickName: params.memberNickName,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setUserNo(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log(params.memberNickName);
+
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          px: "24px",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          pt: "24px",
+        }}
+      >
+        <Typography const="h1">Loading</Typography>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
   return (
-    <div>
+    <Box sx={{ px: "24px", pb: "70px" }}>
       <FollowerProfile />
-    </div>
+      <ProfileView />
+      <ProfilePost userNo={userNo} />
+    </Box>
   );
 };
 
